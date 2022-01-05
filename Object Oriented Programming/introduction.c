55349488e5545fe56f8faa501bd98ef881e1211d
RegalQuail <82899321+RegalQuail@users.noreply.github.com>
2022-01-05 09:19:58 +0100

    OOP

---
+++ Object Oriented Programming/introduction.c
@@ -0,0 +1,10 @@
+/*
+    Object Oriented Programming is a programming style that is intended
+    to make thinking about programming closer to thinking about the real world.
+    In programming, objects are independent units, and each has its own identity,
+    just as objects in the real world do.
+
+    An apple is an object; so is a mug. Each has its unique identity.
+    It's possible to have two mugs that look identical, but they are still separate, unique objects.
+*/
+
---
+++ test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int x) {
+
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (isPalindrome(n)) {
+        cout << n << " is a palindrome";
+    }
+    else {
+        cout << n " is NOT a palindrome";
+    }
+
+    return 0;
+}
